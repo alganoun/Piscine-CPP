964d81e9321a8221b882dd96bd154f20eb4ca9ad
Allan Ganoun <hor4tio.code@gmail.com>
2021-03-18 00:12:15 +0100

    phonebook almost finished : need to verify SEARCH algo

--- module_00/ex01/phonebook.cpp
+++ module_00/ex01/phonebook.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alganoun <alganoun@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: allanganoun <allanganoun@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 08:36:08 by alganoun          #+#    #+#             */
-/*   Updated: 2021/03/16 15:30:09 by alganoun         ###   ########lyon.fr   */
+/*   Updated: 2021/03/18 00:02:57 by allanganoun      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,63 @@ void	contact::get_contact_info(int n)
 {
 	this->contact_n[0] = n + 48;
 	std::cout << "\nEnter CONTACT N." + std::string(this->contact_n) + " informations below :\n"  << std::endl;
-	contact::set_info("\nFirst Name :", this->first_name);
-	contact::set_info("\nLast Name :", this->last_name);
-	contact::set_info("\nNick Name : ", this->nickname);
-	contact::set_info("\nLogin :", this->login);
-	contact::set_info("\nPostal Adress :", this->postal_adress);
-	contact::set_info("\nE-mail Adress :", this->email_adress);
-	contact::set_info("\nPhone Number :", this->phone_number);
-	contact::set_info("\nBirthday Date (MM/JJ/YY):", this->bday_date);
-	contact::set_info("\nFavorite Meal : ", this->fav_meal);
-	contact::set_info("\nUnderwear Color :", this->under_color);
-	contact::set_info("\nDarkest Secret :", this->dark_secrets);
+	contact::set_info("\nFirst Name :", &this->first_name);
+	contact::set_info("\nLast Name :", &this->last_name);
+	contact::set_info("\nNick Name : ", &this->nickname);
+	contact::set_info("\nLogin :", &this->login);
+	contact::set_info("\nPostal Adress :", &this->postal_adress);
+	contact::set_info("\nE-mail Adress :", &this->email_adress);
+	contact::set_info("\nPhone Number :", &this->phone_number);
+	contact::set_info("\nBirthday Date (MM/JJ/YY):", &this->bday_date);
+	contact::set_info("\nFavorite Meal : ", &this->fav_meal);
+	contact::set_info("\nUnderwear Color :", &this->under_color);
+	contact::set_info("\nDarkest Secret :", &this->dark_secrets);
 	std::cout << "\nCONTACT N." + std::string(this->contact_n) + " SUCCESSFULLY ADDED\n"  << std::endl;
 
 }
 
-void	contact::set_info(std::string info, std::string line)
+void	contact::set_info(std::string info, std::string *str)
 {
 	std::cout << info << std::endl;
-	std::getline(std::cin, line);
+	std::getline(std::cin, *str);
+}
+
+void	contact::print_data(std::string data)
+{
+	std::cout << "|";
+	if (data.length() < 10)
+		data.insert(0, 10 - data.length(), ' ');
+	else if (data.length() > 10)
+	{
+		data.insert(9, 1, '.');
+		data.resize(10);
+	}
+	//print_space(10, data.length(), data);
+	std::cout << data;
+}
+
+void	contact::display_contact_list()
+{
+	std::cout << TAB_LINE << std::endl;
+	contact::print_data(this->contact_n);
+	contact::print_data(this->first_name);
+	contact::print_data(this->last_name);
+	contact::print_data(this->login);
+	std::cout << "|";
+
+}
+
+void	contact::display_specific_info()
+{
+	std::cout << TAB_LINE_SMALL << std::endl;
+	contact::print_data(this->postal_adress);
+	std::cout << "|" << std::endl;
+	std::cout << TAB_LINE_SMALL << std::endl;
+	contact::print_data(this->email_adress);
+	std::cout << "|" << std::endl;
+	std::cout << TAB_LINE_SMALL << std::endl;
+	contact::print_data(this->phone_number);
+	std::cout << "|" << std::endl;
 }
 
 int		main()
@@ -63,6 +101,16 @@ int		main()
 				std::cout << MAX_CONTACT << std::endl;
 			else
 				list[index].get_contact_info(index + 1);
+		}
+		else if (line == "SEARCH")
+		{
+			for (int count = 0; count <= index; count++)
+				list[count].display_contact_list();
+			std::cout << SEARCH_MORE << std::endl;
+			std::cout << MENU_BACK << std::endl;
+			std::getline(std::cin, line);
+			if (line.length() == 1 && line[0] - 48 >= 1 && line[0] - 48 <= 8)
+				list[line[0] - 48 - 1].display_specific_info();
 		}
 		else
 			std::cout << COMMAND_ERROR << std::endl;
--- module_00/ex01/phonebook.hpp
+++ module_00/ex01/phonebook.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   phonebook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alganoun <alganoun@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: allanganoun <allanganoun@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 14:08:51 by alganoun          #+#    #+#             */
-/*   Updated: 2021/03/16 15:31:34 by alganoun         ###   ########lyon.fr   */
+/*   Updated: 2021/03/18 00:08:06 by allanganoun      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 
 # include <iostream>
 
-# define WELCOME			"\n---------- Phonebook 1.0 by Alganoun----------\n"
+# define WELCOME			"\n---------- Phonebook 1.0 by Alganoun ----------\n"
 # define ADD_MAN			"- Type ADD to add a contact\n"
 # define SEARCH_MAN			"- Type SEARCH to see all added contact\n"
 # define EXIT_MAN			"- Type EXIT to exit the progam\n"
 # define COMMAND_ERROR		"ERROR : command not found.\n"
+# define SEARCH_MORE		"Type Contact number to get more informations on it"
+# define MENU_BACK			"Type MENU to go to the Main Menu"
 # define ERROR				"Something went wrong with the phonebook."
 # define MAX_CONTACT		"Phonebook max capacity, you can't add any more contacts"
 # define EXIT_WARNING 		"\n/!\\ ALL contacts will be removed, Continue ? (Y/N)"
 # define EXIT_MESSAGE		" Thank you for using Phonebook 1.0, see you soon."
+# define TAB_LINE			"\n---------------------------------------------"
+# define TAB_LINE_SMALL		"----------" // est ce que je dois refaire des colonnes pour cette partie ?
 
 class	contact
 {
@@ -42,8 +46,11 @@ class	contact
 	std::string under_color;
 	std::string dark_secrets;
 
-	void set_info(std::string info, std::string line);
-	void get_contact_info(int n);
+	void		set_info(std::string info, std::string *line);
+	void		get_contact_info(int n);
+	void		display_contact_list();
+	void		print_data(std::string data);
+	void		display_specific_info();
 };
 
 #endif
