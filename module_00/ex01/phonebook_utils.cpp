2a8dbf91c66afbc0c51cf98ef02e1c012838979c
hor4tio <hor4tio.code@gmail.com>
2021-03-23 07:22:06 +0100

    whitespace management done, class attributes private now

--- module_00/ex01/phonebook_utils.cpp
+++ module_00/ex01/phonebook_utils.cpp
@@ -6,7 +6,7 @@
 /*   By: alganoun <alganoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 14:13:06 by alganoun          #+#    #+#             */
-/*   Updated: 2021/03/22 14:23:22 by alganoun         ###   ########lyon.fr   */
+/*   Updated: 2021/03/23 07:18:22 by alganoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,16 @@ int		write_error(std::string str)
 	return (-1);
 }
 
-
-
 int		smart_getline(std::string *str, int option)
 {
 	std::getline(std::cin, *str);
 	if (option == CAPS_ON)
+	{
 		caps_maker(str);
+		for (int i = 0; (*str)[i]; i++)
+			while (((*str)[i] >= 9 && (*str)[i] <= 13) || (*str)[i] == 32)
+				(*str).erase((*str).begin() + i);
+	}
 	if (std::cin.fail() == 1)
 	{
 		std::cout << std::endl;
