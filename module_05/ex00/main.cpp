41080e3a05f6ff9504ddf1acb58ee03f27e05ec4
Allan Ganoun <hor4tio>
2021-04-20 11:09:32 +0200

    module_05 upadated

---
+++ module_05/ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: allanganoun <allanganoun@student.42lyon    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/20 11:04:48 by allanganoun       #+#    #+#             */
+/*   Updated: 2021/04/20 11:09:28 by allanganoun      ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main ()
+{
+    try
+    {
+        Bureaucrat jim("Jim", 102);
+        Bureaucrat bob("Bob", 0);
+        Bureaucrat eric("Eric", 167);
+    }
+    catch
+    {
+        
+    }
+}
