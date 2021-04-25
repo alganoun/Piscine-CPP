cca899f1b840d3dcb99c32ae02b2297e8ef682e7
Allan Ganoun <hor4tio.code@gmail.com>
2021-04-26 00:25:13 +0200

    save

---
+++ module_07/ex00/main.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: allanganoun <allanganoun@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/25 17:46:11 by allanganoun       #+#    #+#             */
+/*   Updated: 2021/04/26 00:03:11 by allanganoun      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T c = a;
+	a = b;
+	b = c;
+}
+
+template <typename T>
+T min(const T &a, const T &b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+template <typename T>
+T max(const T &a, const T &b)
+{
+	if (a < b)
+		return (b);
+	return (a);
+}
+
+int main( void )
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
